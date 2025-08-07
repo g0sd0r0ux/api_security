092b72bac5fa3c8280e7e058e1f8f2f9a42e138c
g0sd0r0ux <eliu.martineez@gmail.com>
2025-08-07 10:10:23 -0400

    first commit

---
+++ src/test/java/com/manage/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manage.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
